286d42be0072f53b413059be9129caa88e98d216
SanjayR857 <sanjayr9586@gmail.com>
2024-12-23 19:43:41 +0530

    23-12-2024

--- basic/17File/file.go
+++ basic/17File/file.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -10,5 +11,28 @@ func main() {
 	fmt.Println("Welcome to files in golang")
 	fmt.Println("Creating a file")
     content := "This needs to go in a file -LearnCode Online.in"
-    os.Create("./my")
+    
+	os.Create("./mylcogofile.txt")
+	file, err := os.Create("./mylcogofile.txt")
+
+	checkNilErr(err)
+	length,err :=io.WriteString(file,content)
+
+	checkNilErr(err)
+		fmt.Println("Length is: ", length)
+
+	defer file.Close()
+	readFile("./mylcogofile.txt")
+}
+
+func readFile(filename string){
+	file, err := os.ReadFile(filename)
+	checkNilErr(err)
+	fmt.Println("Content of the file is: ", string(file))
+}
+
+func checkNilErr(err error){
+	if err != nil{
+		panic(err)
+	}
 }
